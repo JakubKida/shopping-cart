5974c1f02bb5e236a76619d91f5c7bae9da09199
Jakub Kida <JakubJKida@gmail.com>
2021-10-18 17:13:22 +0200

    add spinner to details page

--- src/pages/ProductDetailPage/ProductDetailPage.js
+++ src/pages/ProductDetailPage/ProductDetailPage.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import Footer from "../../components/Footer/Footer";
+import Spinner from "../../components/Spinner/Spinner";
 
 const ProductDetailsPage = ({ match, addToCart }) => {
   const [item, setItem] = useState({});
+
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     fetchItem();
@@ -13,6 +16,7 @@ const ProductDetailsPage = ({ match, addToCart }) => {
     const data = await fetch(`https://fakestoreapi.com/products/${match.params.id}`);
     const newItemData = await data.json();
     setItem(newItemData);
+    setIsLoaded(true);
   };
 
   const onAddToCart = () => {
@@ -21,37 +25,42 @@ const ProductDetailsPage = ({ match, addToCart }) => {
 
   return (
     <div className="product-detail-page">
-      {Object.keys(item).length !== 0 && item.constructor === Object && (
-        <div className="product-card">
-          <div className="product-image">
-            <img src={item.image} alt={item.image}></img>
-          </div>
-          <div className="product-details">
-            <div className="product-details-title">{item.title}</div>
-            <div className="product-details-category">
-              <span>Category: </span>
-              {item.category}
+      {isLoaded ? (
+        Object.keys(item).length !== 0 &&
+        item.constructor === Object && (
+          <div className="product-card">
+            <div className="product-image">
+              <img src={item.image} alt={item.image}></img>
             </div>
-            <div className="product-details-rating">
-              <span>Rating: </span>
-              {item.rating.rate} ({item.rating.count} votes)
-            </div>
-            <div className="product-details-description">
-              <span>
-                Description: <br></br>
-              </span>
-              {item.description}
-            </div>
-            <div className="product-details-price">
-              {item.price}$
-              <div className="add-to-cart" onClick={onAddToCart}>
-                Add to cart
+            <div className="product-details">
+              <div className="product-details-title">{item.title}</div>
+              <div className="product-details-category">
+                <span>Category: </span>
+                {item.category}
+              </div>
+              <div className="product-details-rating">
+                <span>Rating: </span>
+                {item.rating.rate} ({item.rating.count} votes)
+              </div>
+              <div className="product-details-description">
+                <span>
+                  Description: <br></br>
+                </span>
+                {item.description}
+              </div>
+              <div className="product-details-price">
+                {item.price}$
+                <div className="add-to-cart" onClick={onAddToCart}>
+                  Add to cart
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )
+      ) : (
+        <Spinner></Spinner>
       )}
-      <Footer/>
+      <Footer />
     </div>
   );
 };
